1b802e10bdfea30f4908cf87281de0587c54fe7b
Vikram <vikram.wri231@webreinvent.com>
2023-12-04 12:55:31 +0530

    Fixed: aside route

--- skeletons/vaahcms/module-vue3/Vue/components/Aside.vue
+++ skeletons/vaahcms/module-vue3/Vue/components/Aside.vue
@@ -17,7 +17,7 @@ const items = ref([
             {
                 label: 'Dashboard',
                 icon: 'fa-regular fa-chart-bar',
-                to: "/"
+                route: "/"
             },
         ]
     },
@@ -27,7 +27,20 @@ const items = ref([
 <template>
 
     <div v-if="height">
-        <Menu :model="items"  class="w-full" />
+      <Menu :model="items"  class="w-full" >
+        <template #item="{ item, props }">
+          <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+            <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+              <span :class="item.icon" />
+              <span class="ml-2">{{ item.label }}</span>
+            </a>
+          </router-link>
+          <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </template>
+      </Menu>
     </div>
 
 </template>
