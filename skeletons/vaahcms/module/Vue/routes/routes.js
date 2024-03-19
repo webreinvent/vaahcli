72a596b6a519b088027dc079336eed6ddb8a1fac
Pradeep <webreinvent@gmail.com>
2024-03-19 16:53:38 +0530

    removed: old version files

--- skeletons/vaahcms/module/Vue/routes/routes.js
+++
@@ -1,9 +0,0 @@
-let routes=[];
-
-import dashboard from "./routes-dashboard";
-//import other from "./routes-other";
-
-routes = routes.concat(dashboard);
-//routes = routes.concat(other);
-
-export default routes;
