f947f77ac36c90dd3b3462e9555bb3986398ce6a
Vikram Chand <vikram.wri231@webreinvent.com>
2024-03-20 13:35:29 +0530

    Fixed: skeleton path issue in mac

--- src/libraries/Generator.ts
+++ src/libraries/Generator.ts
@@ -297,7 +297,7 @@ export default class Generator {
 
     let destination = this.getFileDestination(file_path);
 
-    let file_readable_path = __dirname+"./../../skeletons/"+file_path;
+    let file_readable_path = __dirname+"/../../skeletons/"+file_path;
 
     let file_content = fs.readFileSync(file_readable_path).toString();
     let parsed_file_content = ejs.render(file_content, this.inputs);
@@ -531,7 +531,7 @@ export default class Generator {
 
       destination = this.getFileDestination(file_path);
 
-      file_readable_path = __dirname+"./../../skeletons/"+file_path;
+      file_readable_path = __dirname+"/../../skeletons/"+file_path;
 
       file_content = fs.readFileSync(file_readable_path).toString();
       parsed_file_content = ejs.render(file_content, this.inputs);
@@ -579,7 +579,7 @@ export default class Generator {
       file_path = file_path.replace(/\\/g, "/");
       destination = this.getFileDestination(file_path);
 
-      file_readable_path = __dirname+"./../../skeletons/"+file_path;
+      file_readable_path = __dirname+"/../../skeletons/"+file_path;
 
       file_content = fs.readFileSync(file_readable_path).toString();
       parsed_file_content = ejs.render(file_content, this.inputs);
